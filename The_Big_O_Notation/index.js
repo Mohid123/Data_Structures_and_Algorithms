898c2686a99db60a582fe98812fd107cd4f4e9bd
Mohid <mohid.dastgeer@solissol.com>
2022-03-17 12:41:41 +0500

    new change

--- The_Big_O_Notation/index.js
+++ The_Big_O_Notation/index.js
@@ -43,3 +43,57 @@ function countUpAndDown(n) {
         console.log(j)
     }
 }
+
+// The Big O of objects
+let instructor = {
+    firstName: "kelly",
+    isInstructor: true,
+    favoriteNumbers: [1,2,3,4] // objects work well when we don't need order or when we need fast access or insertion/removal
+}
+
+console.log(Object.entries(instructor));
+console.log(Object.keys(instructor))
+console.log(Object.values(instructor))
+console.log(instructor.hasOwnProperty("firstName"))
+
+// Big O of Objects is:
+// Insertion: O(1)
+// Removal: O(1)
+// Searching O(n)
+// Access O(1)
+
+// Big O of some Object Methods
+// Object.keys() O(n)
+// Object.values() O(n)
+// Object.entries() O(n)
+// Object.hasOwnProperty() O(1)
+
+// So Objects are very fast and basic but there is no order.
+
+// ARRAYS in Big O
+ let names = ["Mohid", "Junaid", "Sudais"];
+ let values = [true, {}, "string", [], 2];
+// Arrays are intrinsically ordered. This is great but can affect some methods' performance
+
+// Big O of arrays is:
+
+// Access O(1)
+
+// Insertion/Removal It depends on where we perform these operations. If we add an item to the end it's quite fast O(1), but say we add it to the start or somewhere in between,
+//  the indexes get messed up and have to be rearranged and reassigned because remember arrays have ORDER. The amount of time will grow with the size of array. Similar for
+// when we remove an item.
+
+// Searching O(n)
+
+// Methods of arrays
+// push O(1)
+// pop O(1)
+// shift O(n)
+// unshift O(n)
+// concat O(n)
+// slice O(n)
+// splice O(n)
+// sort O(n * log n)
+// forEach/map/filter/reduce O(n)
+
+// push and pop methods are always faster than shift and unshift.
