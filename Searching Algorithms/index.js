f2b6f3c4ba808100936091e031ae88ea82d20cdb
Mohid123 <muhammamohid141@gmail.com>
2023-03-29 12:27:29 +0500

    binary search example

--- Searching Algorithms/index.js
+++ Searching Algorithms/index.js
@@ -12,5 +12,30 @@ function findNumber(arr, val) {
   }
   return -1
 }
+let timeA = performance.now();
+console.log(findNumber([1,2,3,4,5,6,7,8,9, 10, 11, 12, 13 ,14], 14));
+let timeB = performance.now();
+console.log(`Time taken: ${(timeB - timeA) / 1000} seconds`);
 
-console.log(findNumber([1,2,3,4,5], 5))
+// Binary Search 
+
+function findNumberBinary(arr, val) {
+  let left = 0;
+  let right = arr.length - 1;
+  for (let i = 0; i < arr.length; i++) {
+    let middle = Math.floor((left + right) / 2);
+    let currentElem = arr[middle];
+    if(currentElem > val) {
+      right = middle - 1;
+    }
+    else if(currentElem < val) {
+      left = middle + 1
+    }
+    else {return middle}
+  }
+  return -1
+}
+let time1 = performance.now();
+console.log(findNumberBinary([1,2,3,4,5,6,7,8,9, 10, 11, 12, 13, 14], 14));
+let time2 = performance.now();
+console.log(`Time taken: ${(time2 - time1) / 1000} seconds`);
